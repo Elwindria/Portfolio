5688221b242c0cc7a9c4fedf6a9ef09624823e70
Elwindria <p.lopez2000@laposte.net>
2022-08-22 16:59:44 +0200

    ajout du squelette de base de mon site + ajout du fond en three.js

---
+++ js/main.js
@@ -0,0 +1,67 @@
+//====================================================
+// THREE.JS - Fond etoilé en 3D - Animation - Début
+
+let scene, camera, renderer, stars, starGeo;
+
+function init() {
+
+  //nouvelle scene avec la couleur du fond
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x040D20 );
+
+  camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 1;
+  camera.rotation.x = Math.PI/2;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //création d'une géometry avec 2000 coins
+  starGeo = new THREE.Geometry();
+  for(let i=0;i<2000;i++) {
+    star = new THREE.Vector3(
+      Math.random() * 600 - 300,
+      Math.random() * 600 - 300,
+      Math.random() * 600 - 300
+    );
+    star.velocity = 0;
+    star.acceleration = 0.02;
+    starGeo.vertices.push(star);
+  }
+
+  //ajout à chaque coin, la texture star.png
+  let sprite = new THREE.TextureLoader().load( 'images/star.png' );
+  let starMaterial = new THREE.PointsMaterial({
+    //couleur du png + taille
+    color: 0xfbfbfb,
+    size: .6,
+    map: sprite
+  });
+
+  stars = new THREE.Points(starGeo,starMaterial);
+  //ajout du tout à la scène
+  scene.add(stars);
+
+  window.addEventListener("resize", onWindowResize, false);
+
+  animate(); 
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+function animate() {
+
+  //On fait tourner notre forme géométrique et ses 2000coins/stars
+  stars.rotation.y +=0.0003;
+
+  renderer.render(scene, camera);
+  // chargement en boucle de la fonction animate
+  requestAnimationFrame(animate);
+}
+init();
+
+// THREE.JS - Fond etoilé en 3D - Animation - Fin !
+//====================================================
